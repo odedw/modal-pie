ef26ad9d073c58f14e94f77c579c3a09cf82284e
Oded Welgreen <oded.welgreen@gmail.com>
2018-12-25 23:16:28 +0200

    Updates

---
+++ precache-manifest.3e60cf48cebab7f4de1145df37203284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84535db3f1eca6a48ab3",
+    "url": "/modal-pie/static/js/runtime~main.84535db3.js"
+  },
+  {
+    "revision": "b171e8c7c73d776b5e81",
+    "url": "/modal-pie/static/js/main.b171e8c7.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/modal-pie/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "b171e8c7c73d776b5e81",
+    "url": "/modal-pie/static/css/main.bbebce6a.chunk.css"
+  },
+  {
+    "revision": "d990e3b104832de6b7f5be4ff6017b09",
+    "url": "/modal-pie/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/modal-pie/precache-manifest.3e60cf48cebab7f4de1145df37203284.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/modal-pie/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
