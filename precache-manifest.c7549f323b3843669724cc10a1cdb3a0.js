fab577af33e7f21986c4c21b3cbb8386bc060282
Oded Welgreen <oded.welgreen@gmail.com>
2018-12-26 22:07:32 +0200

    Updates

---
+++ precache-manifest.c7549f323b3843669724cc10a1cdb3a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84535db3f1eca6a48ab3",
+    "url": "/modal-pie/static/js/runtime~main.84535db3.js"
+  },
+  {
+    "revision": "61b02676189e8c670201",
+    "url": "/modal-pie/static/js/main.61b02676.chunk.js"
+  },
+  {
+    "revision": "c0c7c9cdbaae4292a9f2",
+    "url": "/modal-pie/static/js/1.c0c7c9cd.chunk.js"
+  },
+  {
+    "revision": "61b02676189e8c670201",
+    "url": "/modal-pie/static/css/main.ffaeda01.chunk.css"
+  },
+  {
+    "revision": "b8201dd325a0879dc11520158f5fd527",
+    "url": "/modal-pie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/modal-pie/precache-manifest.3e60cf48cebab7f4de1145df37203284.js"
+  "/modal-pie/precache-manifest.c7549f323b3843669724cc10a1cdb3a0.js"
 );
 
 workbox.clientsClaim();
