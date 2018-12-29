8a45bccd59d699c965494463e6820a21bbc7c386
Oded Welgreen <oded.welgreen@gmail.com>
2018-12-29 20:08:17 +0200

    try to fix routing?

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import ModesChartView from "./ModesChartView";
 import ModeView from "./ModeView";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <header className="App-header">
-            <Route path={process.env.PUBLIC_URL + '/'} exact component={ModesChartView} />
-            <Route path={process.env.PUBLIC_URL + '/:name'} component={ModeView} />
+            <Route path='/' exact component={ModesChartView} />
+            <Route path='/:name' component={ModeView} />
           </header>
         </div>
       </Router>
